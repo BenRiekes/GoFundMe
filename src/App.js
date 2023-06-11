6f39157ed5884389aee371d1c6f9b1dc97c8b731
BenRiekes <95994689+BenRiekes@users.noreply.github.com>
2023-06-11 15:49:31 -0700

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
+import React from 'react';
+import NavBar from './Components/NavBar';
+
 
 function App() {
   return (
-    <div className="App">
-      <p>hello</p>
+    <div>
+      <NavBar />
     </div>
   );
 }
